269cc7b79783d88a7ed800083ca2dbe0fbdfb193
Daniel Ale <danielale9291@gmail.com>
2018-11-29 11:38:05 +0100

    Completed Sorting and Count

--- src/resolvers/Query.js
+++ src/resolvers/Query.js
@@ -1,11 +1,31 @@
-function feed(parent, args, context, info) {
+async function feed(parent, args, context, info) {
 	const where = args.filter
 		? {
 				OR: [ { url_contains: args.filter }, { description_contains: args.filter } ]
 			}
 		: {};
 
-	return context.db.query.links({ where, skip: args.skip, first: args.first, orderBy: args.orderBy }, info);
+	// 1
+	const queriedLinks = await context.db.query.links(
+		{ where, skip: args.skip, first: args.first, orderBy: args.orderBy },
+		`{ id }`
+	);
+
+	// 2
+	const countSelectionSet = `
+	  {
+		aggregate {
+		  count
+		}
+	  }
+	`;
+	const linksConnection = await context.db.query.linksConnection({}, countSelectionSet);
+
+	// 3
+	return {
+		count: linksConnection.aggregate.count,
+		linkIds: queriedLinks.map((link) => link.id)
+	};
 }
 
 module.exports = {
