4341fe9dc76c86b1989695121e69906e17f516d9
Daniel Ale <danielale9291@gmail.com>
2018-11-29 15:52:20 +0100

    COmpleted search

--- client/src/components/CreateLink.js
+++ client/src/components/CreateLink.js
@@ -1,6 +1,7 @@
 import React, { Component } from "react";
 import { Mutation } from "react-apollo";
 import gql from "graphql-tag";
+import { FEED_QUERY } from "./LinkList";
 
 const POST_MUTATION = gql`
 	mutation PostMutation($description: String!, $url: String!) {
